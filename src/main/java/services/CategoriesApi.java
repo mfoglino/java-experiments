b7846c11d8acaad1ebe32bdd77a396ccc6e43884
ext_mfoglino <ext_mfoglino@mercadolibre.com>
2014-11-02 21:05:32 -0300

    show level

--- src/main/java/services/CategoriesApi.java
+++ src/main/java/services/CategoriesApi.java
@@ -44,25 +44,27 @@ public class CategoriesApi {
 		all.addAll(categsLevel1);
 
 		for (Category category : categsLevel1) {
-			List<Category> categories = getAllCategoriesAux(category);
+			List<Category> categories = getAllCategoriesAux(category,2);
 			all.addAll(categories);
 		}
 
 		return all;
 	}
 
-	public List<Category> getAllCategoriesAux(Category node) {
+	public List<Category> getAllCategoriesAux(Category node, int level) {
 		List<Category> all = new ArrayList<Category>();
 
 		List<Category> categoriesForThisLevel = getChildrenCategoriesFor(node.getId());
 		//all.addAll(categoriesForThisLevel);
 
+		level++;
+		System.out.println("Level:" + level);
 		System.out.println("Parent:" + node);
 		all.add(node);
-		for (Category category : categoriesForThisLevel) {		
+		for (Category category : categoriesForThisLevel) {
 			
 			System.out.println("Children:"+category);
-			List<Category> categories = getAllCategoriesAux(category);
+			List<Category> categories = getAllCategoriesAux(category, level);
 			all.addAll(categories);
 		}
 
